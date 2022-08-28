5c104070121ecc414ced5eded8c2f6556a03b7b9
Avishay Bikowsky <avishayhome@Avishays-MBP.lan>
2022-08-28 21:54:54 +0300

    alert that bypasses mm with readable text for 2 funs

--- public/content.js
+++ public/content.js
@@ -1,11 +1,15 @@
 const oldFun = window.ethereum.request;
-console.log('bfnim');
-const dangerous_methods = ['eth_sign', 'personal_sign'];
+const dangerous_methods = ['eth_sign', 'personal_sign', 'eth_accounts'];
 window.ethereum.request = (x) => {
   if(x.method && dangerous_methods.indexOf(x.method) > -1 ){
-    alert('tizaer ya zabal');
+    alert(`Alert: this action ${methodToDescription[x.method]}`);
   }
-  // If you stay here more than 30 seconds then you migth get called again
+
   console.log(JSON.stringify(x));
   return oldFun(x);
+}
+
+const methodToDescription = {
+  "eth_accounts": "will return the address of the most recently used account on your metamask extension",
+  "eth_sign": "can be used to get your approval on transactions, or any other data, making it a dangerous",
 }
